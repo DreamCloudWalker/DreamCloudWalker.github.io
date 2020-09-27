a5025f25aa03cd6012ccd030c285ecdd5f1801bd
JianDeng <dengjian@garena.com>
2020-09-27 17:05:32 +0800

    [Add] add texture to vertexAnim

--- vertexAnim.js
+++ vertexAnim.js
@@ -2,7 +2,9 @@ const DEGREE_TO_RADIUS = Math.PI / 180;
 const CAMERA_POSITION = vec3.fromValues(0.0, 0.0, 10.0);
 const PLANE_WIDTH = 3;
 const PLANE_HEIGHT = 3;
+var mPlaneTexture = null;
 var mVertices = [];
+var mTexCoods = [];
 var mViewportWidth = 0;
 var mViewportHeight = 0;
 var mPitching = 0.0;
@@ -47,6 +49,8 @@ function main() {
     // as the destination to receive the result.
     mProjectionMatrix = mat4.create();
     mat4.perspective(mProjectionMatrix, fov, aspect, zNear, zFar);
+
+    mPlaneTexture = loadTexture(mGl, './texture/bg_sky.jpg');
 
     // init shader
     updateShader();
@@ -95,6 +99,7 @@ function updateShader() {
         program: shaderProgram,
         attribLocations: {
             vertexPosition: mGl.getAttribLocation(shaderProgram, 'aPosition'),
+            textureCoord: mGl.getAttribLocation(shaderProgram, 'aTexCoord')
         },
         uniformLocations: {
             uProjectionMatrixHandle: mGl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
@@ -102,7 +107,8 @@ function updateShader() {
             uViewMatrixHandle: mGl.getUniformLocation(shaderProgram, 'uViewMatrix'),
             uWidthSpanHandle: mGl.getUniformLocation(shaderProgram, 'uWidthSpan'),
             uHeightSpanHandle: mGl.getUniformLocation(shaderProgram, 'uHeightSpan'),
-            uAngleHandle: mGl.getUniformLocation(shaderProgram, 'uAngle')
+            uAngleHandle: mGl.getUniformLocation(shaderProgram, 'uAngle'),
+            uTexSamplerHandle: mGl.getUniformLocation(shaderProgram, 'uTexSampler')
         },
     };
 }
@@ -129,7 +135,10 @@ function loadShader(mGl, type, source) {
 
 function initBuffers(gl) {
     // Now create an array of positions for the plane
-    mVertices = createPlane(3, 3, 8, 8);
+    const rows = 8;
+    const cols = 8;
+    mVertices = createPlaneVertices(4, 3, rows, cols);
+    mTexCoods = generateTexCoord(rows, cols);
 
     // Create a buffer for the sphere's positions.
     const positionBuffer = gl.createBuffer();
@@ -137,12 +146,78 @@ function initBuffers(gl) {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mVertices), gl.STATIC_DRAW);
 
+    // Create a buffer for the viewFrustum's color.
+    const uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mTexCoods), gl.STATIC_DRAW);
+
     return {
-        position: positionBuffer
+        position: positionBuffer,
+        uv: uvBuffer
     };
 }
 
-function createPlane(width, height, rows, cols) {
+// Initialize a texture and load an image.
+// When the image finished loading copy it into the texture.
+function loadTexture(gl, url) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+  
+    // Because images have to be download over the internet
+    // they might take a moment until they are ready.
+    // Until then put a single pixel in the texture so we can
+    // use it immediately. When the image has finished downloading
+    // we'll update the texture with the contents of the image.
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const width = 1;
+    const height = 1;
+    const border = 0;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    const pixel = new Uint8Array([255, 255, 255, 255]);
+    // 1表示翻转，0表示不翻转，参考 https://juejin.im/post/5d4423c4f265da038f47ef87
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); 
+    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  width, height, border, srcFormat, srcType,
+                  pixel);
+  
+    const image = new Image();
+    image.onload = function() {
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                    srcFormat, srcType, image);
+  
+      // WebGL1 has different requirements for power of 2 images
+      // vs non power of 2 images so check if the image is a
+      // power of 2 in both dimensions.
+      if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+         // Yes, it's a power of 2. Generate mips.
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+         gl.generateMipmap(gl.TEXTURE_2D);
+      } else {
+         // No, it's not a power of 2. Turn off mips and set
+         // wrapping to clamp to edge
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+      }
+    };
+    image.src = url;
+  
+    return texture;
+}
+
+function isPowerOf2(value) {
+    return (value & (value - 1)) == 0;
+}
+
+function createPlaneVertices(width, height, rows, cols) {
     const widthSize = width / rows;
     const heightSize = height / cols;
     // const vCnt = cols * rows * 6;
@@ -183,6 +258,40 @@ function createPlane(width, height, rows, cols) {
     return vertices;
 }
 
+function generateTexCoord(rows, cols) {
+    var result = [];
+    var sizeW = 1.0 / rows;
+    var sizeH = 1.0 / cols;
+    var index = 0;
+    for(var i = 0; i < cols; i++) {
+        for(var j = 0; j < rows; j++) {
+            var s = j * sizeW;
+            var t = i * sizeH;
+            
+            result[index++] = s;
+            result[index++] = t;
+            
+            result[index++] = s;
+            result[index++] = t + sizeH;
+            
+            result[index++] = s + sizeW;
+            result[index++] = t;
+            
+            
+            result[index++] = s + sizeW;
+            result[index++] = t;
+            
+            result[index++] = s;
+            result[index++] = t + sizeH;
+            
+            result[index++] = s + sizeW;
+            result[index++] = t + sizeH;    			
+        }
+    }
+
+    return result;
+}
+
 function onKeyPress(event) {
     switch (String.fromCharCode(event.keyCode)) {
         case 'P':
@@ -211,7 +320,7 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
     // Update the rotation for the next draw
     // mYawing -= deltaTime * 0.38;
     // mPitching += deltaTime * 0.1;
-    mAngle += 0.3 * (Math.PI / 16);
+    mAngle += 0.6 * (Math.PI / 16);
 
     gl.clearColor(1.0, 1.0, 1.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
@@ -256,9 +365,37 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
         gl.enableVertexAttribArray(
             programInfo.attribLocations.vertexPosition);
     }
+
+    // Tell WebGL how to pull out the texture coordinates from the texture 
+    // coordinate buffer into the textureCoord attribute.
+    {
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.uv);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.textureCoord,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.textureCoord);
+    }
     
     // Tell WebGL to use our program when drawing
     gl.useProgram(programInfo.program);
+
+    // Specify the diffuseTexture to map onto the faces.
+    // Tell WebGL we want to affect diffuseTexture unit 0
+    gl.activeTexture(gl.TEXTURE0);
+    // Bind the diffuseTexture to diffuseTexture unit 0
+    gl.bindTexture(gl.TEXTURE_2D, mPlaneTexture);
+    // Tell the shader we bound the diffuseTexture to diffuseTexture unit 0
+    gl.uniform1i(programInfo.uniformLocations.uTexSamplerHandle, 0);
 
     // Set the shader uniforms
     gl.uniformMatrix4fv(
@@ -276,4 +413,7 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 
     const offset = 0;
     gl.drawArrays(gl.TRIANGLES, offset, mVertices.length / 3);
+
+    gl.disableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+    gl.disableVertexAttribArray(programInfo.attribLocations.textureCoord);
 }
